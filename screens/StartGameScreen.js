57233649a605ff2410192248cdea859258a0392c
Dan Possehl <danielbpossehl@gmail.com>
2021-12-15 08:23:18 -0700

    second component built

---
+++ screens/StartGameScreen.js
@@ -0,0 +1,20 @@
+import React from "react";
+import { View, StyleSheet, Text } from "react-native";
+
+const StartGameScreen = () => {
+  return (
+    <View style={styles.screen}>
+      <Text>Please Work</Text>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  screen: {
+    flex: 1,
+    padding: 10,
+    alignItems: "center",
+  },
+});
+
+export default StartGameScreen;
