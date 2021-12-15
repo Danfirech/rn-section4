f626420cf15a5b37f6b6228432cddf6202877a7e
Dan Possehl <danielbpossehl@gmail.com>
2021-12-15 08:35:26 -0700

    added styling to ButtonContainer

--- screens/StartGameScreen.js
+++ screens/StartGameScreen.js
@@ -1,10 +1,18 @@
 import React from "react";
-import { View, StyleSheet, Text } from "react-native";
+import { View, StyleSheet, Text, TextInput, Button } from "react-native";
 
 const StartGameScreen = () => {
   return (
     <View style={styles.screen}>
-      <Text>Please Work</Text>
+      <Text style={styles.title}>Start a New Game!</Text>
+      <View style={styles.inputContainer}>
+        <Text>Select A Number</Text>
+        <TextInput />
+        <View style={styles.buttonContainer}>
+          <Button title="reset" onPress={() => {}} />
+          <Button title="confirm" onPress={() => {}} />
+        </View>
+      </View>
     </View>
   );
 };
@@ -14,6 +22,23 @@ const styles = StyleSheet.create({
     flex: 1,
     padding: 10,
     alignItems: "center",
+  },
+  buttonContainer: {
+    flexDirection: "row",
+    width: "100%",
+    justifyContent: "space-between",
+    paddingHorizontal: 15,
+  },
+
+  inputContainer: {
+    width: "80%",
+    maxWidth: "80%",
+    alignItems: "center",
+  },
+
+  title: {
+    fontSize: 20,
+    marginVertical: 10,
   },
 });
 
